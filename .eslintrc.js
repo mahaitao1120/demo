bad67a8a5057c34a9ea4f72f694a56e4bc52ac1e
马海涛 <rlx@appledeMacBook-Air-7.local>
2020-10-20 09:56:22 +0800

    ma

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,35 +21,18 @@ module.exports = {
   },
   rules: {
     'prettier/prettier': 'off',
-    //强制使用单引号
-    // "quotes": ['error', 'single'],
-    // //强制不使用分号结尾
-    // "semi": ['error', 'never'],
-    // 'prettier/prettier': 'off',
-    // 'no-console': 'off',
-    // //  'generator-star-spacing': 0,
-    // 'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    // 'no-unused-vars': 'off',
-    // 'vue/max-attributes-per-line': 'off',
-    // 'vue/attributes-order': 'off',
-    // 'vue/html-closing-bracket-newline': 'off',
-    // 'vue/require-default-prop': 'off',
-    // 'vue/order-in-components': 'off',
-    // 'vue/html-self-closing': 'off',
-    // 'vue/attribute-hyphenation': 'off',
-    // 'vue/html-quotes': 'off',
-    // 'vue/multiline-html-element-content-newline': 'off',
-    // 'vue/name-property-casing': 'off',
-    // 'vue/require-prop-types': 'off',
-    // 'vue/no-unused-components': 'off',
-    // //  "vue/no-use-v-if-with-v-for": "off",
-    // 'vue/no-v-html': 'off',
-    // 'no-unexpected-multiline': 'off',
-    // 'vue/no-side-effects-in-computed-properties': 'off',
-    // 'vue/no-template-shadow': 'off',
-    // 'no-undef': 'off',
-    // 'no-constant-condition': 'off',
-    // 'no-case-declarations': 'off',
-    // 'vue/valid-template-root': 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		// allow paren-less arrow functions
+		'arrow-parens': 0,
+		// allow async-await
+		'generator-star-spacing': 0,
+		// allow debugger during development
+		'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+		'no-useless-escape': 0,
+		'no-tabs': 0,
+		'indent': 0,
+		'object-curly-spacing': 0,
+		'space-before-function-paren': 0
   },
 }
