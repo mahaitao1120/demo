080dfe5cfe2e558bfbdbdffff016757ae98993ab
马海涛 <rlx@appledeMacBook-Air-7.local>
2020-10-20 09:50:58 +0800

    ma

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
-let a = "1111";
-let b = "111aaa1";
-console.log(a);
-console.log(b);
+Vue.config.productionTip = false
+let a = '1111'
+let b = '111aaaaaa1'
+console.log(a)
+console.log(b)
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: (h) => h(App)
+}).$mount('#app')
