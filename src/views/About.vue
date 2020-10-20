e749bfa4385a78cf50180b4e8b9f702c2d66070d
马海涛 <rlx@appledeMacBook-Air-7.local>
2020-10-20 09:40:49 +0800

    ma

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,15 +6,15 @@
 
 <script>
 export default {
-  name: 'Aaa',
+  name: "Aaa",
   methods: {
-    test () {
-      let a = 'aaabb'
+    test() {
+      let a = "aaaaaabb";
       // 测试案例
-      console.log('aaaa')
-      console.log(a)
+      console.log("aaaa");
+      console.log(a);
       // 测试案例
-    }
-  }
-}
+    },
+  },
+};
 </script>
